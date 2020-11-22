8d40c156224a36c43f3bf0cefe33c7b41f7adc31
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-11-22 14:04:10 +0100

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,9 +1,3 @@
-/* var scroll_links = document.querySelector(".bdt-news-ticker-content ul li a");
-
-for (var i=0; i<scroll_links.length; i++) {
-    scroll_links[i].removeAttribute("href");
-} */
-
 // ES6 Solution
 var scroll_links = Array.from(document.querySelectorAll(".bdt-news-ticker .bdt-news-ticker-content ul li a").forEach(function(link) {
     while (link.firstChild) {
