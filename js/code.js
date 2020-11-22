b5f100028ecd08899af7cdde3d719a806207c944
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-11-22 14:11:28 +0100

    Update code.js
    plain js also works fine

--- js/code.js
+++ js/code.js
@@ -1,7 +1,16 @@
 // ES6 Solution
-var scroll_links = Array.from(document.querySelectorAll(".bdt-news-ticker .bdt-news-ticker-content ul li a").forEach(function(link) {
+/* 
+var scroll_links = Array.from(document.querySelectorAll(".bdt-news-ticker .bdt-news-ticker-content ul li a").forEach(function (link) {
     while (link.firstChild) {
         link.parentNode.insertBefore(link.firstChild, link);
     }
     link.parentNode.removeChild(link);
 }));
+ */
+
+// Plain JS Solution (works)
+var all_links = document.querySelectorAll(".bdt-news-ticker .bdt-news-ticker-content ul li a");
+
+for(var i=0; i<all_links.length; i++){
+    all_links[i].removeAttribute("href");
+}
